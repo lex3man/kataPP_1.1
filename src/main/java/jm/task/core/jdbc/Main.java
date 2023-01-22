444f5f4a358084786c920933cbd64cd2178b8b0e
master <master@lic.arinteg.ru>
2023-01-22 12:58:00 +0300

    Main class crear

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -8,18 +8,5 @@ public class Main {
 
     public static void main(String[] args) {
         userService.createUsersTable();
-
-        userService.saveUser("Джо", "Байден", (byte) 78);
-        userService.saveUser("Трамп", "Дональд", (byte) 74);
-        userService.saveUser("Барак", "Обама", (byte) 59);
-        userService.saveUser("Джордж", "Буш", (byte) 74);
-
-        userService.removeUserById(2);
-
-        userService.getAllUsers();
-
-        userService.cleanUsersTable();
-
-        userService.dropUsersTable();
     }
 }
